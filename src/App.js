a3adb0cdaf35fd2f04b2c9e4f6f53134cc348344
Mona Herter <hertermona@gmail.com>
2023-06-17 17:18:19 +0200

    add api and delete function

--- src/App.js
+++ src/App.js
@@ -3,19 +3,63 @@ import Form from "./components/Form.js";
 import { uid } from "uid";
 import List from "./components/List.js";
 import useLocalStorageState from "use-local-storage-state";
+import { useEffect, useState } from "react";
 
 export default function App() {
   const [activities, setActivity] = useLocalStorageState("activities", {
     defaultValue: [],
   });
+  const [weather, setWeather] = useState({});
 
   function handleAddActivity(newActivity) {
     setActivity([...activities, { ...newActivity, id: uid() }]);
   }
+
+  function handleDeleteActivity(id) {
+    setActivity(activities.filter((activity) => activity.id !== id));
+  }
+
+  let filteredActivities = activities.filter(
+    (activity) => activity.isForGoodWeather === weather.isGoodWeather
+  );
+
+  useEffect(() => {
+    async function loadWeather() {
+      try {
+        const response = await fetch(
+          "https://example-apis.vercel.app/api/weather"
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          setWeather(data);
+          console.log(data);
+          console.log("test");
+        } else {
+          console.error("Response not okay");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    loadWeather();
+  }, []);
+
+  if (!weather) {
+    return <p>Loading..</p>;
+  }
+
   return (
     <div className="App">
+      <h2>
+        {weather.condition} {weather.temperature}°C
+      </h2>
+      <List
+        activities={filteredActivities}
+        isGoodWeather={weather.isGoodWeather}
+        onDeleteActivity={handleDeleteActivity}
+      />
       <Form onAddActivity={handleAddActivity} />
-      <List activities={activities} />
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,21 @@
-export default function List({ activities }) {
+export default function List({ activities, isGoodWeather, onDeleteActivity }) {
   return (
-    <ul>
-      {activities.map((activity) => (
-        <li key={activity.id}>{activity.name}</li>
-      ))}
-    </ul>
+    <>
+      {isGoodWeather ? (
+        <p>Good weather activites:</p>
+      ) : (
+        <p>Bad weather activites</p>
+      )}
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>
+            {activity.name}
+            <button onClick={() => onDeleteActivity(activity.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
